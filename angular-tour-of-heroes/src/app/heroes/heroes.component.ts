7f88ac219f0681478ccc30e8121056419dc920bf
Movileanu Miruna <mirunamovi@gmail.com>
2024-01-18 20:46:04 +0200

    HeroDetailComponents

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
-import { Hero } from '../hero';
+import {Component} from '@angular/core';
+// import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
+// import {FormsModule} from '@angular/forms';
+
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
+  // standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  // imports: [
+  //   FormsModule,
+  //   NgIf,
+  //   NgFor,
+  //   UpperCasePipe,
+  // ],
 })
+
 export class HeroesComponent {
-  /*
-  hero = 'Windstorm';
-  */
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  
+  onSelected(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
